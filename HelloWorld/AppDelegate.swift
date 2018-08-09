c34e1cee058800948ec14575bb1e982152401a49
Xiao Yao <yaoxiao89@gmail.com>
2018-08-08 20:46:44 -0400

    Setup project

---
+++ HelloWorld/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  
+//
+//  Copyright © 2018 Xiao Yao. All rights reserved.
+//  See LICENSE.txt for licensing information
+//
+
+import UIKit
+
+// MARK: - AppDelegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder {
+
+    var window: UIWindow?
+
+}
+
+// MARK: - UIApplicationDelegate
+
+extension AppDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
